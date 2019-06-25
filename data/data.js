f65ad707f2bc419feaef7649fd2f16204dd14100
aminvafaei13@gmail.com <aminvafaei13@gmail.com>
2019-06-25 12:31:05 +0430

    add new version site

---
+++ data/data.js
@@ -0,0 +1,104 @@
+
+// Change your profile data here
+let profile = {
+  // picture: "/img/me.jpg",
+  name: "Mohammad Amin Vafaei",
+  description:
+    "I'm a Backend developer primarily working with NodeJs and NoSQL Databases.",
+  bio:
+    "I've been programming for 6 years.i'm a Back-end  developer with experience creating, maintaining and implementing everything from databases to web pages. I nerd out when learning new programming languages functionality feels giddy when finding /destroying bugs. I like to face new challenges in every field in my  career and I'm not afraid of them.",
+  email: "aminvafaei13@gmail.com",
+  phone: "+98 938 917 8071 ",
+  location: "Tehran, Iran",
+  birth: "1992-01-25", // YYYY-MM-DD format
+  skills: [
+    "PHP",
+    "Laravel",
+    "NodeJs",
+    "ExpressJs",
+    "AdonisJs",
+    "Restful Api",
+    "Web Socket",
+    "Linux",
+    "Mysql",
+    "PostgreSQL",
+    "MongoDB",
+    "Redis",
+    "Git",
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/mohammad-amin-vafaei-3a95b0147",
+    // twitter: "https://twitter.com/GuimGonzalez",
+    github: "https://github.com/Mohammad-e-amin",
+    // stackoverflow: "https://stackoverflow.com/users/8700961/guim-gonzalez",
+  },
+  experience: [
+    {
+      title: "Full Stack Developer",
+      company: "Mashhad Electric Energy Distribution Co.",
+      url: "http://portal.meedc.net/",
+      location: "Mashhad, Iran",
+      description:
+        "Implementation enterprise integration portal (eip) with php and angular v 1.5",
+      from: "Jun 2014",
+      to: "Oct 2017"
+    },
+    {
+      title: "Full Stack Developer",
+      company: "Yaico",
+      url: "http://www.kbkco.net",
+      location: "Tehran, Iran",
+      description:
+        "Implementation KALARESAN web application (for transfer packages between Iran cities) with laravel and vue js",
+      from: "Dec 2017",
+      to: "Apr 2018"
+    },
+    {
+      title: "Backend Developer ",
+      company: "Media Hamrah Ertebatat",
+      url: "",
+      location: "Tehran, Iran",
+      description:
+        "Implementation notification panel for MCI with nodeJS,laravel,mongodb.",
+      from: "Apr 2018",
+      to: "Dec 2018"
+    },
+    {
+      title: "Backend Developer ",
+      company: "AppTech Co.",
+      url: "http://www.app-tech.ir",
+      location: "Tehran, Iran",
+      description:
+        "",
+      from: "Dec 2018",
+      to: "Present"
+    }
+  ],
+  education: [
+    {
+      school: "University of Guilan",
+      degree: "Engineering",
+      fieldOfStudy: "Software Engineering",
+      location: "Rasht, Guilan, Iran",
+      from: "2010",
+      to: "2014"
+    }
+  ],
+  quote: "But it works on my machine."
+};
+
+let page = {
+  language: "english",
+  // ^ languages suported:
+  //  - english (default)
+  //  - spanish
+  //  - italian
+  //  - catalan
+  //  - french
+  theme: "nightsky"
+  // ^ themes suported:
+  //  - nightsky (default)
+  //  - sunset
+  //  - purpledream
+  //  - cutekitty
+};
