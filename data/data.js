b72edd3c17a54db710366367d83dc04ae915531a
aminvafaei13@gmail.com <aminvafaei13@gmail.com>
2019-07-22 12:15:03 +0430

    update git url

--- data/data.js
+++ data/data.js
@@ -33,7 +33,7 @@ let profile = {
   social: {
     linkedin: "https://www.linkedin.com/in/mohammad-amin-vafaei-3a95b0147",
     twitter: "https://twitter.com/Mohammad_e_Amin",
-    github: "https://github.com/Mohammad-e-amin",
+    github: "https://github.com/mavafaei",
     stackoverflow: "https://stackoverflow.com/users/7088841/mohammad-amin",
   },
   experience: [
