0556cfe8d3e05fc7451c6ee303a9bf6205bab153
aminvafaei13 <aminvafaei13@gmail.com>
2020-02-09 20:04:14 +0330

    update info

--- data/data.js
+++ data/data.js
@@ -1,113 +1,124 @@
 
 // Change your profile data here
-let profile = {
+const profile = {
   // picture: "/img/me.jpg",
-  name: "Mohammad Amin Vafaei",
+  name: 'Mohammad Amin Vafaei',
   description:
     "I'm a Backend developer primarily working with NodeJs and NoSQL Databases.",
   bio:
     "I've been programming for 6 years.i'm a Back-end  developer with experience creating, maintaining and implementing everything from databases to web pages. I nerd out when learning new programming languages functionality feels giddy when finding /destroying bugs. I like to face new challenges in every field in my  career and I'm not afraid of them.",
-  email: "aminvafaei13@gmail.com",
-  phone: "+98 938 917 8071 ",
-  location: "Tehran, Iran",
-  birth: "1992-01-25", // YYYY-MM-DD format
+  email: 'aminvafaei13@gmail.com',
+  phone: '+98 938 917 8071 ',
+  location: 'Tehran, Iran',
+  birth: '1992-01-25', // YYYY-MM-DD format
   skills: [
 
-    "Node.Js",
-    "Express Js",
-    "Koa 2",
-    "Adonis Js",
-    "Feathers Js",
-    "DRF",
-    "PHP",
-    "Laravel",
-    "Restful API",
-    "MicroService",
-    "gRPC",
-    "Web Socket",
-    "Linux",
-    "Mysql",
-    "PostgreSQL",
-    "MongoDB",
-    "RethinkDB",
-    "ElasticSearch",
-    "Redis",
-    "Git",
+    'Node.Js',
+    'Express Js',
+    'Koa 2',
+    'Adonis Js',
+    'Feathers Js',
+    'PHP',
+    'Laravel',
+    'Hyperledger Fabric',
+    'Restful API',
+    'MicroService',
+    'gRPC',
+    'Web Socket',
+    'Linux',
+    'Mysql',
+    'PostgreSQL',
+    'MongoDB',
+    'RethinkDB',
+    'ElasticSearch',
+    'Redis',
+    'Git'
   ],
   social: {
-    linkedin: "https://www.linkedin.com/in/mohammad-amin-vafaei-3a95b0147",
-    twitter: "https://twitter.com/Mohammad_e_Amin",
-    github: "https://github.com/mavafaei",
-    stackoverflow: "https://stackoverflow.com/users/7088841/mohammad-amin",
-    blog: "https://virgool.io/@aminvafaei13"
+    linkedin: 'https://www.linkedin.com/in/mohammad-amin-vafaei-3a95b0147',
+    twitter: 'https://twitter.com/Mohammad_e_Amin',
+    github: 'https://github.com/mavafaei',
+    stackoverflow: 'https://stackoverflow.com/users/7088841/mohammad-amin',
+    blog: 'https://virgool.io/@aminvafaei13'
   },
   experience: [
     {
-      title: "Full Stack Developer",
-      company: "Mashhad Electric Energy Distribution Co.",
-      url: "http://portal.meedc.net/",
-      location: "Mashhad, Iran",
+      title: 'Full Stack Developer',
+      company: 'Mashhad Electric Energy Distribution Co.',
+      url: 'http://portal.meedc.net/',
+      location: 'Mashhad, Iran',
       description:
-        "Implementation enterprise integration portal (eip) with php and angular v 1.5",
-      from: "Jun 2014",
-      to: "Oct 2017"
+        'Implementation enterprise integration portal (eip) with php and angular v 1.5',
+      from: 'Jun 2014',
+      to: 'Oct 2017'
     },
     {
-      title: "Full Stack Developer",
-      company: "Yaico",
-      url: "http://www.kbkco.net",
-      location: "Tehran, Iran",
+      title: 'Full Stack Developer',
+      company: 'Yaico',
+      url: 'http://www.kbkco.net',
+      location: 'Tehran, Iran',
       description:
-        "Implementation KALARESAN web application (for transfer packages between Iran cities) with laravel and vue js",
-      from: "Dec 2017",
-      to: "Apr 2018"
+        'Implementation KALARESAN web application (for transfer packages between Iran cities) with laravel and vue js',
+      from: 'Dec 2017',
+      to: 'Apr 2018'
     },
     {
-      title: "Backend Developer ",
-      company: "Media Hamrah Ertebatat",
-      url: "",
-      location: "Tehran, Iran",
+      title: 'Backend Developer ',
+      company: 'Media Hamrah Ertebatat',
+      url: '',
+      location: 'Tehran, Iran',
       description:
-        "Implementation notification panel for MCI with nodeJS,laravel,mongodb.",
-      from: "Apr 2018",
-      to: "Dec 2018"
+        'Implementation notification panel for MCI with nodeJS,laravel,mongodb.',
+      from: 'Apr 2018',
+      to: 'Dec 2018'
     },
     {
-      title: "Backend Developer ",
-      company: "AppTech Co.",
-      url: "http://www.app-tech.ir",
-      location: "Tehran, Iran",
+      title: 'Backend Developer ',
+      company: 'AppTech Co.',
+      url: 'http://www.app-tech.ir',
+      location: 'Tehran, Iran',
       description:
-        "",
-      from: "Dec 2018",
-      to: "Present"
+        '',
+      from: 'Dec 2018',
+      to: 'Nov 2019'
+    },
+    {
+      title: 'Backend and Blockchain Developer ',
+      company: 'Ariatak',
+      url: '',
+      location: 'Tehran, Iran',
+      description:
+        'Design and implement blockchain based service for iran banking network.',
+      from: 'Nov 2019',
+      to: 'Present'
     }
+
   ],
   education: [
     {
-      school: "University of Guilan",
-      degree: "Engineering",
-      fieldOfStudy: "Software Engineering",
-      location: "Rasht, Guilan, Iran",
-      from: "2010",
-      to: "2014"
+      school: 'University of Guilan',
+      degree: 'Engineering',
+      fieldOfStudy: 'Software Engineering',
+      location: 'Rasht, Guilan, Iran',
+      from: '2010',
+      to: '2014'
     }
   ],
-  quote: "But it works on my machine."
-};
+  quote: 'But it works on my machine.'
+}
 
-let page = {
-  language: "english",
+const page = {
+  language: 'english',
   // ^ languages suported:
   //  - english (default)
   //  - spanish
   //  - italian
   //  - catalan
   //  - french
-  theme: "nightsky"
+  theme: 'nightsky'
   // ^ themes suported:
   //  - nightsky (default)
   //  - sunset
   //  - purpledream
   //  - cutekitty
-};
+}
